e4f15761e290860c932be3e326e29cec8ae12d54
doublefint <doublefint@gmail.com>
2021-09-30 14:05:12 +0300

    simplify

--- main.go
+++ main.go
@@ -1,27 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type task struct {
-	input   string
-	err     error
-	decimal interface{}
+const (
+	maxLength = 8
+)
+
+var (
+	errMaxLength  = errors.New("unexpected length")
+	errWrongInput = errors.New("unexpected input")
+)
+
+type input string
+
+func (in input) maxLength() error {
+	if len(in) > maxLength {
+		return fmt.Errorf("%v: %v, but max length is: ", errMaxLength, len(in), maxLength)
+	}
+	return nil
+}
+
+func (in input) valid() error {
+	for i, bit := range in {
+		if (bit != '0') && (bit != '1') {
+			return fmt.Errorf("%v: %v at position %v", errWrongInput, bit, i)
+		}
+	}
+	return nil
+}
+
+func (in input) convert() (uint8, error) {
+	if err := in.maxLength(); err != nil {
+		return 0, err
+	}
+	if err := in.valid(); err != nil {
+		return 0, err
+	}
+	var out uint8
+	last := len(in) - 1
+	for i, bit := range in {
+		if bit == '1' {
+			out |= 1 << (last - i)
+		}
+	}
+	return out, nil
 }
 
 func main() {
-	var process task
+
+	var in input // = input("10")
 
 	fmt.Print("Enter a sequence consisting of input digits (0 or 1), up to 8 characters: ")
-	fmt.Scanf("%s", &process.input)
+	fmt.Scanf("%s", &in)
 
-	if err := check(process.input); err != nil {
-		process.err = err
-		process.decimal = "failed"
-		fmt.Printf("Entered sequence: %s, conversion result: %v, error: %#v\n",
-			process.input, process.decimal, process.err)
-	} else {
-		process.decimal = bin2dec(process.input)
-		fmt.Printf("Entered sequence: %s, conversion result: %v\n",
-			process.input, process.decimal)
+	out, err := in.convert()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	fmt.Printf(" %s converted to: %d\n", in, out)
+
 }
