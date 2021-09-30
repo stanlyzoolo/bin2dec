5afad67c03c0d8f672c821835a4732e9ffb97c32
stanlyzoolo <stanlyzoolo@gmail.com>
2021-09-30 12:18:46 +0300

    output messages fixed

--- main.go
+++ main.go
@@ -11,16 +11,17 @@ type task struct {
 func main() {
 	var process task
 
-	fmt.Print("Enter a checked consisting of input digits (0 or 1), up to 8 characters: ")
+	fmt.Print("Enter a sequence consisting of input digits (0 or 1), up to 8 characters: ")
 	fmt.Scanf("%s", &process.input)
 
 	if err := check(process.input); err != nil {
 		process.err = err
 		process.decimal = "failed"
+		fmt.Printf("Entered sequence: %s, conversion result: %v, error: %#v\n",
+			process.input, process.decimal, process.err)
 	} else {
 		process.decimal = bin2dec(process.input)
+		fmt.Printf("Entered sequence: %s, conversion result: %v\n",
+			process.input, process.decimal)
 	}
-
-	fmt.Printf("Output:\nchecked: %s, conversion result: %v, error: %#v\n",
-		process.input, process.decimal, process.err)
 }
